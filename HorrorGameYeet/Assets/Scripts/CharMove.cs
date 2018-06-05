00e94aeba339daf48231af8ad02a7cde077091db
atholt95 <32171853+atholt95@users.noreply.github.com>
2018-06-04 21:33:49 -0500

    CameraScript

---
+++ HorrorGameYeet/Assets/Scripts/CharMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMove : MonoBehaviour {
+
+    Rigidbody _rigidBody;
+
+    public float speed = 20f;
+
+    public Camera cam;
+	// Use this for initialization
+	void Awake () {
+        _rigidBody = GetComponent<Rigidbody>();
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 right = cam.transform.right;
+        Vector3 forward = Vector3.Cross(right, Vector3.up);
+
+        Vector3 movement = Vector3.zero;
+
+        movement += right * (Input.GetAxis("Horizontal") * speed);
+        movement += forward * (Input.GetAxis("Vertical") * speed);
+
+        _rigidBody.AddForce(movement, ForceMode.VelocityChange);
+    }
+}
