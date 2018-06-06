0dda5e5c6f69d0817363b64ae21f48f865a6fee7
Joshua Estill <jestill@mail.bradley.edu>
2018-06-05 21:13:15 -0400

    Updates to Player
    Added new test scene, and created new movement script

---
+++ HorrorGameYeet/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private CharacterController controller;
+
+    public float moveSpeed;
+
+    private Vector3 moveDirection;
+
+    public Transform pivot;
+
+    public float rotateSpeed;
+
+    public GameObject playerModel;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        // moveDirection.y = yStore;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+	}
+}
